f9cefd1e5ed1f291d0b6760b71f802caabc4c1ce
jomorespi <jordi.javajan@gmail.com>
2019-12-23 17:40:47 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    var $active_tab = $('section.active').attr('id');
+    
+    $('#app-header h1 span').html($active_tab);
+    
+    $('#index-link').click(function(){
+        $(this).addClass('active');
+        $('#skills-link').removeClass('active');
+        $('#experience-link').removeClass('active');
+        
+        $('section#index').addClass('active');
+        $('section#skills').removeClass('active');
+        $('section#experience').removeClass('active');
+
+        $('#app-header h1 span').html('index.html');
+    });
+    
+    $('#skills-link').click(function(){
+        $(this).addClass('active');
+        $('#index-link').removeClass('active');
+        $('#experience-link').removeClass('active');
+        
+        $('section#skills').addClass('active');
+        $('section#index').removeClass('active');
+        $('section#experience').removeClass('active');
+
+        $('#app-header h1 span').html('skills.css');
+    });
+    
+    $('#experience-link').click(function(){
+        $(this).addClass('active');
+        $('#index-link').removeClass('active');
+        $('#skills-link').removeClass('active');
+        
+        $('section#experience').addClass('active');
+        $('section#skills').removeClass('active');
+        $('section#index').removeClass('active');
+
+        $('#app-header h1 span').html('experience.js');
+    });
+    
+});
