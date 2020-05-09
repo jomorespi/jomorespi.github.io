48dafb6833a733aada0f5177f271fb48c2d7eb19
jomorespi <jordi.javajan@gmail.com>
2020-05-09 19:14:14 +0200

    Add interactions

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,48 @@ $(document).ready(function(){
         $(this).addClass('active');
         $('#' + link).addClass('active');
     });
+
+    $('#app-header > div').mouseover(function(){
+        $('#app-header > div').addClass('hover');
+    });
+
+    $('#app-header > div').mouseout(function(){
+        $('#app-header > div').removeClass('hover');
+    });
+
+    $('#app-header > div:first-child').click(function(){
+        $('#tabs-container').hide();
+        $('#desktop-icon').addClass('show');
+    });
+    
+    $('#app-header > div:nth-child(2)').click(function(){
+        $('#tabs-container').addClass('minimized');
+        $('#dock').addClass('show');
+    });
+
+    $('#app-header > div:nth-child(3)').click(function(){
+        $('#tabs-container').toggleClass('maximized');
+    });
+
+    $('#dock img').click(function(){
+        $('#tabs-container').removeClass('minimized');
+        $('#dock').removeClass('show');
+    });
+
+    $('#desktop-icon').click(function(){
+        $(this).addClass('focus');
+    });
+
+    $('#desktop-icon').dblclick(function(){
+        $(this).removeClass('show');
+        $(this).removeClass('focus');
+        $('#tabs-container').show();
+    });
+
+    $(document).on("click",function(e) {
+        var desktopicon = $("#desktop-icon");
+            if (!desktopicon.is(e.target) && desktopicon.has(e.target).length === 0) { 
+                $('#desktop-icon').removeClass('focus');
+            }
+   });
 });
